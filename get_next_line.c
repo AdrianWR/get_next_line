bb1746cef3fd75122a762e8bec165aceb4e8518e
Adrian Roque <aroque@c1r9p2.42sp.org.br>
2020-02-16 21:34:47 -0300

    Update algorithm

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,47 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 11:50:29 by aroque            #+#    #+#             */
-/*   Updated: 2020/02/14 16:43:47 by aroque           ###   ########.fr       */
+/*   Updated: 2020/02/16 21:32:39 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <string.h>
+#include <stdio.h>
 
-static 
+char	*get_buffer(fd)
+{
+	int		gnl;
+	char	*buffer;
+
+	if (!(buffer = malloc((BUFFER_SIZE + 1) * sizeof(*buffer))))
+		return (NULL);
+	gnl = read(fd, buffer, BUFFER_SIZE);
+	buffer[BUFFER_SIZE] = '\0';
+	return (buffer);
+}
 
 int	get_next_line(int fd, char **line)
 {
-	int	gnl;
+	int i;
+	//int end;
 	char *buffer;
 
-	while ((gnl = read(fd, buffer, BUFFER_SIZE)))
+	i = 0;
+	*line = malloc(BUFFER_SIZE + 1);
+	buffer = get_buffer(fd);
+	printf("Buffer: %s\n", buffer);
+	while (!memccpy(*line, buffer, LBREAK, BUFFER_SIZE))
 	{
-
+		buffer = get_buffer(fd);
+		
 	}
-	gnl = read(fd, buffer, BUFFER_SIZE);
-	line[BUFFER_SIZE + 1] = '\0';
-	if (gnl < 1)
-		return (gnl);
+	//end = ((char *)- *line);
+	//printf("end: %d\n", end);
+	//(*line)[10] = '\0';
+	printf("line: %s\n", *line);
+	//*line[i + 1] = '\0';
+	//if (gnl < 1)
+	//	return (gnl);
 	return (1);
 }
