7b7f3efcdf7ab04e794a768cb008bfe5bb87f6e6
Adrian Roque <adrian.w.roque@gmail.com>
2020-02-17 19:48:03 -0300

    Remove memory allocation at test

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: aroque <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 19:30:38 by aroque            #+#    #+#             */
-/*   Updated: 2020/02/16 21:32:42 by aroque           ###   ########.fr       */
+/*   Updated: 2020/02/17 17:59:32 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,16 @@
 int	main(int argc, char *argv[])
 {
 	int file;
-	int gnl;
 	char *line;
 
 	if (argc == 2)
 	{
 		file = open(argv[1], O_RDONLY);
-		printf("File: %d\n", file);
-		gnl = get_next_line(file, &line);
-		printf("Retorno: %d\n", gnl);
+		get_next_line(file, &line);
 		printf("%s\n", line);
+		free(line);
+		get_next_line(file, &line);
+		printf("%s\n", line);
+		free(line);
 	}
 }
