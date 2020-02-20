de777daef4f16a03093c5efdc74d2408833657c7
Adrian Roque <adrian.w.roque@gmail.com>
2020-02-20 16:56:55 -0300

    Add strdel function and return definitions

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,33 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 11:50:29 by aroque            #+#    #+#             */
-/*   Updated: 2020/02/19 18:09:38 by aroque           ###   ########.fr       */
+/*   Updated: 2020/02/20 11:45:02 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int	print_line(int rd, char **heap, char **line)
+static void	ft_strdel(char **str)
+{
+	if (*str && str)
+	{
+		free(*str);
+		*str = NULL;
+	}
+}
+
+static int	print_line(int rd, char **heap, char **line)
 {
 	int			i;
 	char		*tmp;
 
 	i = 0;
 	if (rd < 0)
-		return (rd);
+		return (GNL_FAILURE);
 	else if (rd == 0 && *heap[0] == '\0')
-		return (rd);
-	while ((*heap)[i] != LBREAK || !(*heap)[i])
+		return (GNL_EOF);
+	while ((*heap)[i] != LBREAK && (*heap)[i] != '\0')
 		i++;
 	if ((*heap)[i] == LBREAK)
 	{
@@ -32,7 +41,15 @@ int	print_line(int rd, char **heap, char **line)
 		free(*heap);
 		*heap = tmp;
 	}
-	return (1);
+	else
+	{
+		*line = ft_strdup(*heap);
+	}
+	if ((*heap)[0] == '\0')
+	{
+		ft_strdel(heap);
+	}
+	return (GNL_SUCCESS);
 }
 
 int	get_next_line(int fd, char **line)
@@ -43,14 +60,16 @@ int	get_next_line(int fd, char **line)
 	static char	*heap;
 
 	tmp = NULL;
+	if (fd < 0 || !line)
+		return (GNL_FAILURE);
 	if (!heap)
 	{
 		if (!(heap = malloc(sizeof(*heap))))
-			return (-1);
+			return (GNL_FAILURE);
 		*heap = '\0';
 	}
 	if (!(buffer = malloc((BUFFER_SIZE + 1) * sizeof(*buffer))))
-		return (-1);
+		return (GNL_FAILURE);
 	while ((rd = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[rd] = '\0';
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: aroque <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 19:30:38 by aroque            #+#    #+#             */
-/*   Updated: 2020/02/19 18:18:22 by aroque           ###   ########.fr       */
+/*   Updated: 2020/02/20 11:28:03 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,18 @@ int	main(int argc, char *argv[])
 	int file;
 	char *line;
 
-	i = 0;
+	i = -1;
 	if (argc == 3)
 	{
 		file = open(argv[1], O_RDONLY);
-		while (i < atoi(argv[2]))
+		while (++i < atoi(argv[2]))
 		{
 			ret = get_next_line(file, &line);
-			//printf("Retorno: %d\n", ret);
 			if (ret > 0)
 			{
 				printf("%s\n", line);
 				free(line);
 			}
-			i++;
 		}
 	}
 }
